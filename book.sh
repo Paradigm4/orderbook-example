41c987749123ca2f6c255d6b4d1e291f93f71d8c
B. W. Lewis <blewis@illposed.net>
2014-06-22 11:02:09 -0400

    Added redimension in load.sh

--- book.sh
+++ book.sh
@@ -6,7 +6,15 @@
 
 iquery -aq "load_library('orderbook')" || echo "Error: please install the orderbook aggregate. See https://github.com/Paradigm4/orderbook-example"
 iquery -naq "remove(book)" 2>/dev/null
-q="store(variable_window(symbol_time, ms, 1, 0, orderbook(order_record)), book)"
+# Prepare the book entries and redimension into a more reasonable chunk size
+# (the orderbook aggregate has special chunk size requirements)
+#
+# NOTE! Be careful about chunk size selection in the redimension.
+# SciDB wants a chunk size that, on average, has about a million non-empty
+# cells per chunk or so. It's better to err on the high side here than
+# to create an array with tons of chunks with hardly anything in them.
+#
+q="store(redimension(variable_window(symbol_time, ms, 1, 0, orderbook(order_record)),<order_record_orderbook:string null> [symbol_index=0:*,100,0,ms=0:86399999,10000000,0]), book)"
 iquery -naq "$q"
 
 echo
